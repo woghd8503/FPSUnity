89aedddb54d07371b6169b2255b9d039a0ce220e
James Park <woghd8503@naver.com>
2020-10-06 23:05:17 +0900

    우측으로 이동하기 Vector3.right/Vector3.left/Vector3.up/Vector3.down

---
+++ csPlayerMove.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csPlayerMove : MonoBehaviour
+{
+ 
+    void Update()
+    {
+        transform.Translate(Vector3.right * 5 * Time.deltaTime);
+    }
+}
