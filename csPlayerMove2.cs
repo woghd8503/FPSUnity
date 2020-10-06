06e17aa2a7c5d88d7197ac5f596d76b5b09cc4ae
James Park <woghd8503@naver.com>
2020-10-06 23:38:16 +0900

    앞, 뒤, 좌, 우 사용자가 입력한 방향키에 따라 이동

---
+++ csPlayerMove2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Input class : 사용자의 입력에 따른 방향 설정 등..
+// 3D에서는 물체를 배치하기 위한 좌표계가 그 축에 따라 왼속 좌표계와 오른손 좌표계로 나뉜다. XYZ
+public class csPlayerMove2 : MonoBehaviour
+{
+    // 플레이어 이동할 속력
+    public float speed = 5;
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dir = Vector3.right * h + Vector3.up * v;
+        transform.Translate(dir * speed * Time.deltaTime);
+    }
+}
